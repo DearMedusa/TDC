c46889dc340611e8cfaec0c6a3ff5eccce4120b9
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-03-17 14:32:07 +0100

    Update tablesdemult.c

--- tablesdemult.c
+++ tablesdemult.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
-int main(){
-  int a = 2;
-  int b = 2;
-  while(1){
-    printf("%d x %d", a,b);
-  }
-//si signal d'arret (ctrl c) afficher le résultat
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
+int score;
+
+void handler(int signal){
+  printf("Le signal %d a bien été reçu\n", signal);
+  printf("Score: %d\n", score);
+}
+
+int main(){
+  struct sigaction action;
+  int i;
+  action.sa_handler = handler;
+  action.sa_flags = 0;
+  sigemptyset(&action.sa_mask);
+  sigaction(SIGINT,&action,NULL);
+  sigaction(SIGQUIT,&action,NULL);
+  sigaction(SIGTERM, &action, NULL);
+
+  while(1)
+
+    for(i = 0; i < 10; i++){
+      printf("a x b\n");
+      scanf();
+      if (correcte alors) score + 1 = score;
+    }
+
+  return 0;
 }
